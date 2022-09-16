6b94a8ba3d3f45edff47bdc4ecf56b3b11c405a6
s1avyan <s1avyan27@gmail.com>
2022-09-16 17:55:20 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,6 +1,5 @@
 const refs = {
   divForWork: document.querySelector("#boxes"),
-  divForRemove: document.querySelector("#boxes, div"),
   btnForAdd: document.querySelector("[data-create]"),
   btnForDel: document.querySelector("[data-destroy]"),
   inputField: document.querySelector("input"),
@@ -22,10 +21,13 @@ function addDiv() {
 }
 
 function deleteDiv() {
-  while (refs.divForWork.firstChild) {
-    const firstChild = refs.divForWork.firstChild;
-    refs.divForWork.firstChild.remove(firstChild);
-  }
+  // refs.divForWork.querySelectorAll("*").forEach((div) => div.remove());
+  // -------------------------------------------------------
+  // while (refs.divForWork.firstChild) {
+  //   refs.divForWork.removeChild(refs.divForWork.firstChild);
+  // }
+  // -------------------------------------------------------
+  refs.divForWork.innerHTML = "";
 }
 
 function createDivMassive() {
@@ -34,12 +36,16 @@ function createDivMassive() {
   for (let i = 0; i < refs.inputField.value; i += 1) {
     const divCreate = document.createElement("div");
     divForCreate.push(divCreate);
-    divCreate.style.width += 30 + i * 10 + "px";
-    divCreate.style.height += 30 + i * 10 + "px";
+    divCreate.style.width += funcForWithAndHeigth(i);
+    divCreate.style.height += funcForWithAndHeigth(i);
     divCreate.style.backgroundColor = getRandomHexColor();
     console.log(divCreate.style.backgroundColor);
   }
   return divForCreate;
+}
+function funcForWithAndHeigth(i) {
+  let value = i * 10 + 30 + "px";
+  return value;
 }
 
 function getRandomHexColor() {
